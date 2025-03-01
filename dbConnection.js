17c2386b3f86500786c76baf526909114009767f
darshilll <darsgajul2003@gmail.com>
2025-03-01 14:38:41 +0530

    initial commit

--- dbConnection.js
+++ dbConnection.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 
-const dbConnection = async() => {
-     try {
-          mongoose.connect(process.env.DB_CONNECTION)
-          console.log("MongoDB Connected");
-          
-     } catch (error) {
-          console.log("Error" + error);    
-     }
-}
+const dbConnection = async () => {
+  try {
+    mongoose.connect(process.env.DB_CONNECTION);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log("Error" + error);
+  }
+};
 
 export default dbConnection;
